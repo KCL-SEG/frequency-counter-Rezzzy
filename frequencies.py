c3c55ad8856399f0afdb21424640588fecbff099
Rez <rez-tal@outlook.com>
2022-10-13 21:27:12 +0100

    Add str check to frequencies function

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,11 @@ def frequencies(items):
     _dict = {}
 
     for item in items:
+        item = str(item)
         if not item in _dict:
             _dict[item] = 0
         _dict[item] += 1
 
     return _dict
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
